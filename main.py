b2124759072349a4de35fe222dd707b4dab779d6
Jam516 <kufuorkofi@gmail.com>
2023-09-25 18:47:23 +0000

    commit

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ hop_bridge_pct = 0.01
 
 
 @st.cache_resource(ttl=3600, show_spinner="Fetching onchain data...")
-def getDuneData():
+def get24hTxnData():
   query = QueryBase(
       name="op_txn_stats",
       query_id=3036014,
@@ -50,6 +50,30 @@ def getMedGas():
   dune = DuneClient(os.environ["DUNE_API_KEY"])
   results = dune.refresh_into_dataframe(query)
   return results
+
+
+def set_pct_distribution(df):
+  df['pct_distribution'] = 0
+  df.loc[df['tx_type'] == 'eth_transfer',
+         'pct_distribution'] = eth_transfer_pct
+  df.loc[df['tx_type'] == 'erc20_transfer',
+         'pct_distribution'] = erc20_transfer_pct
+  df.loc[df['tx_type'] == 'uniswap_trade',
+         'pct_distribution'] = uniswap_trade_pct
+  df.loc[df['tx_type'] == 'hop_bridge', 'pct_distribution'] = hop_bridge_pct
+
+
+def calculate_daily_revenue(df, num_txns):
+  return num_txns * (df.apply(
+      lambda row: row['med_l2_rev'] * row['pct_distribution'], axis=1).sum())
+
+
+def calculate_daily_cost(df, med_gas_price):
+  df['daily_l1_gas_used_inbox'] = df.apply(
+      lambda row: regression_model(row['med_calldata_bytes'], row[
+          'med_l1_gas_used'], num_txns * row['pct_distribution']),
+      axis=1)
+  return (med_gas_price * df['daily_l1_gas_used_inbox'].sum()) / 1e9
 
 
 def regression_model(calldata_bytes_per_user_tx, calldata_gas_per_user_tx,
@@ -93,50 +117,46 @@ st.title('OP Calculator 🔴✨')
 st.subheader('Estimate the profitability of a new OP stack rollup')
 
 num_txns = st.number_input("Enter your estimated number of daily transactions",
-                           value=10000,
-                           placeholder="Type a number...")
+                           value=10000)
 
 run = st.button("PREDICT", type="primary")
 
 if run:
-  df = getDuneData()
+  try:
+    past24h_txn_data = get24hTxnData()
+  except Exception as e:
+    st.error(f"Error fetching Dune data: {e}")
+    raise e
 
   st.subheader(
       "Step 1: Pull revenue and cost for a median transaction (past 24h)")
-  st.dataframe(df)
+  st.dataframe(past24h_txn_data)
 
   st.subheader("Step 2: Assume % distribution of txn types")
-  df['pct_distribution'] = 0
-  df.loc[df['tx_type'] == 'eth_transfer',
-         'pct_distribution'] = eth_transfer_pct
-  df.loc[df['tx_type'] == 'erc20_transfer',
-         'pct_distribution'] = erc20_transfer_pct
-  df.loc[df['tx_type'] == 'uniswap_trade',
-         'pct_distribution'] = uniswap_trade_pct
-  df.loc[df['tx_type'] == 'hop_bridge', 'pct_distribution'] = hop_bridge_pct
+  set_pct_distribution(past24h_txn_data)
   st.write("ETH Transfers = 31% of txns")
   st.write("ERC20 Transfers = 19% of txns")
   st.write("Uniswap Trades = 49% of txns")
   st.write("Hop Bridge Out = 1% of txns")
 
   st.subheader("Step 3: Predict daily revenue for the rollup")
-  st.caption("Sum of (txn revenue * number of transactions) for each transaction type")
-  daily_rev = num_txns * (df.apply(
-      lambda row: row['med_l2_rev'] * row['pct_distribution'], axis=1).sum())
-  st.write("Predicted Daily Revenue = " + str(round(daily_rev,3)) + "ETH")
+  st.caption(
+      "Sum of (txn revenue * number of transactions) for each transaction type"
+  )
+  daily_rev = calculate_daily_revenue(past24h_txn_data, num_txns)
+  st.write(f"Predicted Daily Revenue = {round(daily_rev, 3)} ETH")
 
   st.subheader("Step 4: Predict daily cost for the rollup")
-  st.caption("Regression Model is used to calculate the Gas used on L1. This is multiplied by the median gas price of the past day.")
-  df['daily_l1_gas_used_inbox'] = df.apply(lambda row: regression_model(
-      row['med_calldata_bytes'], row['med_l1_gas_used'],
-      (num_txns * row['pct_distribution'])),
-                                           axis=1)
-
+  st.caption(
+      "Regression Model is used to calculate the Gas used on L1. This is multiplied by the median gas price of the past day."
+  )
   med_gas_price = getMedGas()['median_gas_price_gwei'][0]
-  daily_cost = (med_gas_price * df['daily_l1_gas_used_inbox'].sum()) / 1e9
-  st.write("Predicted Daily Cost = " + str(round(daily_cost,3)) + "ETH")
+  daily_cost = calculate_daily_cost(past24h_txn_data, med_gas_price)
+  st.write(f"Predicted Daily Cost = {round(daily_cost, 3)} ETH")
 
   st.subheader('Output', divider='rainbow')
-  st.caption("Predicted Daily Profit = Predicted Daily Revenue - Predicted Daily Cost")
-  st.subheader("Predicted Daily Profit = " + str(round((daily_rev - daily_cost),2)) +
-               "ETH")
+  st.caption(
+      "Predicted Daily Profit = Predicted Daily Revenue - Predicted Daily Cost"
+  )
+  daily_profit = daily_rev - daily_cost
+  st.subheader(f"Predicted Daily Profit = {round(daily_profit, 2)} ETH")
